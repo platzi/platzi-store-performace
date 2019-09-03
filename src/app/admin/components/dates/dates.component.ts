512dc3ad755dfa467f560a61dae1b9a43c754cda
Nicolas Molina <nicolas.molina.monroy@gmail.com>
2019-09-03 14:32:22 -0500

    🚧 work

--- src/app/admin/components/dates/dates.component.ts
+++ src/app/admin/components/dates/dates.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as moment from 'moment';
+
 @Component({
   selector: 'app-dates',
-  templateUrl: './dates.component.html',
+  template: `<h1>{{ date }}</h1>`,
   styleUrls: ['./dates.component.scss']
 })
 export class DatesComponent implements OnInit {
 
+  date: string;
+
   constructor() { }
 
   ngOnInit() {
+    this.date = moment().add(20, 'days').format('YYYY/MMMM/DD');
   }
 
 }
--- src/app/admin/components/pdf/pdf.component.ts
+++ src/app/admin/components/pdf/pdf.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import * as jsPDF from 'jspdf';
 
 @Component({
   selector: 'app-pdf',
-  templateUrl: './pdf.component.html',
+  template: `
+    <h1>pdf</h1>
+    <button type="button" (click)="generatePdf()">build pdf</button>
+  `,
   styleUrls: ['./pdf.component.scss']
 })
 export class PdfComponent implements OnInit {
@@ -10,6 +14,12 @@ export class PdfComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  generatePdf() {
+    const doc = new jsPDF();
+    doc.text('Hello world!', 10, 10);
+    doc.save('a4.pdf');
   }
 
 }

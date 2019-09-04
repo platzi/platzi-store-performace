a8c41d5b53100f3065b4a1bfa5abd529e494ca96
Nicolas Molina <nicolas.molina.monroy@gmail.com>
2019-09-04 15:03:32 -0500

    🚧 work

--- extend.webpack.config.js
+++ extend.webpack.config.js
@@ -6,14 +6,14 @@ module.exports = {
     new GuessPlugin({
       // Alternatively you can provide a Google Analytics View ID
       GA: '201366991',
+      // reportProvider() {
+      //   return Promise.resolve(JSON.parse(require('fs').readFileSync('./routes.json')));
+      // },
       runtime: {
         delegate: false
       },
       routeProvider() {
         return parseRoutes('.');
-      },
-      routeFormatter(path) {
-        return path.replace(/^\/app/, '');
       }
     })
   ]

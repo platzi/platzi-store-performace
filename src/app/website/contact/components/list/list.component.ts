6d0d69a9059819d0a98ca36acd416958e7c49454
Nicolas Molina <nicolas.molina.monroy@gmail.com>
2019-09-04 18:11:09 -0500

    🚧 work

--- src/app/website/contact/components/list/list.component.ts
+++ src/app/website/contact/components/list/list.component.ts
@@ -1,11 +1,20 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output, ChangeDetectionStrategy } from '@angular/core';
 
 import { EmployeeData } from './../../../../core/models/employee.model';
+
+
+const fibonacci = (num: number): number => {
+  if (num === 1 || num === 2) {
+    return 1;
+  }
+  return fibonacci(num - 1) + fibonacci(num - 2);
+};
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ListComponent implements OnInit {
 
@@ -24,6 +33,11 @@ export class ListComponent implements OnInit {
   addItem() {
     this.add.emit(this.label);
     this.label = '';
+  }
+
+  calc(item: EmployeeData) {
+    console.log('list', this.title);
+    return fibonacci(item.num);
   }
 
 }
